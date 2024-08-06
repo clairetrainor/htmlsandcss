9f2de00dffc340db410f6f6763ff6c42bbd4ac0a
Claire Trainor <trainorcn@g.cofc.edu>
2024-08-06 14:36:51 -0400

    updated css, working on external links

--- login.js
+++ login.js
@@ -1,3 +1,14 @@
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });
+});
+
+const arr= ["google.com", "youtube.com", "twitter.com"];
+
+function randSite(){
+    document.getElementById("random").href= "http://" + arr[Math.floor(Math.random()*arr.length)];
+    return false;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("random").addEventListener("click", randSite);
+})
