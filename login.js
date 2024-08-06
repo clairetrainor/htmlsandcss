6803ecadcb614d30d444159dd7e25b3edae12640
Claire Trainor <trainorcn@g.cofc.edu>
2024-08-06 14:51:07 -0400

    updated code, create main page

--- login.js
+++ login.js
@@ -2,13 +2,24 @@ $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
 
-const arr= ["google.com", "youtube.com", "twitter.com"];
+const arr= ["https://www.google.com", "https://www.youtube.com", "https://www.twitter.com"];
 
 function randSite(){
-    document.getElementById("random").href= "http://" + arr[Math.floor(Math.random()*arr.length)];
+    document.getElementById("random").href= arr[Math.floor(Math.random()*arr.length)];
     return false;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("random").addEventListener("click", randSite);
-})
+document.getElementById("random").addEventListener("click", randSite());
+
+function goLogin() {
+    window.location.href="login.html";
+}
+
+document.getElementById("createButton").addEventListener("click",goLogin())
+
+
+function gotoMain() {
+window.location.href="mainPage.html";
+}
+
+document.getElementById("loginButton").addEventListener("click", gotoMain());
